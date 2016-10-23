b1404ad017aa10f2f3528a804165612c9aa20609
Kai Pruess <me@kaipruess.de>
2016-10-23 16:15:10 +0200

    Add chat parsing to conversation

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -1,5 +1,29 @@
 class Conversation < ApplicationRecord
   has_many :messages
 
-  scope :with_messages, -> { includes(messages: [:channel, :participant]) }
+  attr_accessor :content
+
+  before_save :process_content, if: :has_content?
+
+  def self.with_messages
+    includes(messages: [:channel, :participant])
+  end
+
+  def has_content?
+    content.present?
+  end
+
+  private
+
+  def process_content
+    result = chat_parser.parse(content)
+
+    self.note = note.to_s + result.note
+    messages << result.messages
+    self.content = nil
+  end
+
+  def chat_parser
+    @_chat_parser ||= ChatParser::Parser.new(ChatParser::Formats::GENERIC)
+  end
 end
--- spec/models/conversation_spec.rb
+++ spec/models/conversation_spec.rb
@@ -4,4 +4,44 @@ describe Conversation, type: :model do
   it "is valid" do
     expect(build(:conversation)).to be_valid
   end
+
+  describe "#content=" do
+    it "parses out messages and note" do
+      content = "Some content\n[foo]: this is so bar\n[joe]: yup"
+      conversation = build(:conversation, note: nil, content: content)
+
+      conversation.save
+
+      expect(conversation.note).to eq "Some content\n"
+      expect(conversation.messages.count).to eq 2
+    end
+
+    it "adds parsed note to existing" do
+      conversation = build(:conversation,
+                           note: "Special chat\n",
+                           content: "This is special")
+
+      conversation.save
+
+      expect(conversation.note).to eq "Special chat\nThis is special\n"
+    end
+
+    it "does not change the conversation without content" do
+      conversation = build(:conversation, note: nil, content: nil)
+      dup = conversation.dup
+
+      conversation.save
+
+      expect(conversation.note).to eq dup.note
+      expect(conversation.messages).to eq dup.messages
+    end
+
+    it "resets content after parsing" do
+      conversation = build(:conversation, content: "Some content")
+
+      conversation.save
+
+      expect(conversation.content).to be_nil
+    end
+  end
 end
